ebbedd6bba2c555642e24aaac4fe6711c026b20d
Samin Yasar <yasarsamin57@gmail.com>
2025-10-28 16:14:33 +0600

    dev: default redirect function added in the app root layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,5 @@
-import Layout from 'components/layout';
-import { useRouter } from 'expo-router';
-import { useEffect } from 'react';
-import { Image, View } from 'react-native';
+import { Redirect } from 'expo-router';
 
-export default function App() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.replace('/home');
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, [router]);
-
-  return (
-    <Layout>
-      <View className="row table h-screen items-center justify-center">
-        <Image
-          source={require('assets/images/adaptive-icon.png')}
-          className="h-[200px] w-[200px]"
-        />
-      </View>
-    </Layout>
-  );
+export default function Page() {
+  return <Redirect href="/splash" />;
 }
